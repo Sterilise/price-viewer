e176026bfa3f262a319b9220a97486b453590841
Sterilise <36371103+Sterilise@users.noreply.github.com>
2023-05-27 16:33:22 +0100

    set up base for project

---
+++ back-end/src/main/java/com/start/priceviewer/PriceviewerApplication.java
@@ -0,0 +1,13 @@
+package com.start.priceviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceviewerApplication.class, args);
+	}
+
+}
